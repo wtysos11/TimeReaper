2ebc498e59fc79a8e82fec4f7497148a0ca372f6
wty <wtysos11@163.com>
2018-05-26 21:37:27 +0800

    初始框架

---
+++ TimeReaper/Classes/ListItem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeReaper.Classes
+{
+    class ListItem
+    {
+        private string id;
+        private bool isCompleted;
+
+        public string getId() { return id; }
+        public void setID(string _id)
+        {
+            this.id = _id;
+        }
+        public string title { get; set; }//标题
+        public string notes { get; set; }//备注
+        public DateTime deadline { get; set; }//ddl
+
+        public bool completed
+        {
+            get { return isCompleted; }
+            set { isCompleted = value; }
+        }
+
+        public void setComplete(int i)
+        {
+            if (i == 1)
+                isCompleted = true;
+            else
+                isCompleted = false;
+        }
+
+        public void SetTime(string deadline)
+        {
+            DateTimeFormatInfo dateFormat = new DateTimeFormatInfo();
+            dateFormat.ShortDatePattern = "yyyy/MM/dd";
+            DateTime nowTime = Convert.ToDateTime(deadline, dateFormat);
+            this.deadline = nowTime;
+        }
+
+        public ListItem(string title, string deadline,string notes="")
+        {
+            this.id = Guid.NewGuid().ToString(); //生成id
+            this.title = title;
+            SetTime(deadline);
+            this.isCompleted = false; //默认为未完成
+            this.notes = notes;
+        }
+        public ListItem(string id, string title, string deadline, string notes = " ")
+        {
+            this.id = id; //生成id
+            this.title = title;
+            SetTime(deadline);
+            this.isCompleted = false; //默认为未完成
+            this.notes = notes;
+        }
+    }
+}
---
+++ TimeReaper/Classes/TimeReaperManager.cs
@@ -0,0 +1,104 @@
+﻿using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeReaper.Classes
+{
+    class TimeReaperManager
+    {
+        private SQLiteConnection conn;
+        private ObservableCollection<ListItem> allItems = new ObservableCollection<ListItem>();
+        public ObservableCollection<ListItem> AllItems { get { return this.allItems; } }
+
+        private ListItem selectedItem;
+        public ListItem SelectedItem { get { return selectedItem; } set { this.selectedItem = value; } }
+
+        public bool first = false;
+        private static TimeReaperManager _instance;
+        public static TimeReaperManager getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new TimeReaperManager();
+            }
+
+            return _instance;
+        }
+
+        public TimeReaperManager()
+        {
+            conn = new SQLiteConnection("timeReaper.db");
+            using (var statement = conn.Prepare("CREATE TABLE IF NOT EXISTS todolist (id CHAR(36),title VARCHAR(255),notes VARCHAR(255),deadline DATE, PRIMARY KEY (id));"))
+            {
+                statement.Step();
+            }
+            using (var statement = conn.Prepare("SELECT * FROM todolist;"))
+            {
+                while (statement.Step() == SQLiteResult.ROW)
+                {
+                    ListItem item = new ListItem((string)statement[0], (string)statement[1], (string)statement[2], (string)statement[3]);//id title deadline notes
+                    this.allItems.Add(item);
+                }
+            }
+        }
+
+        public void AddTodoItem(string title, string notes, string time)
+        {
+
+            this.allItems.Add(new ListItem(title, notes, time));
+            ListItem item = allItems[allItems.Count - 1];
+            using (var statement = conn.Prepare("INSERT INTO todolist VALUES(?,?,?,?)"))
+            {
+                statement.Bind(1, item.getId());
+                statement.Bind(2, item.title);
+                statement.Bind(3, item.notes);
+
+                DateTimeFormatInfo dateFormat = new DateTimeFormatInfo();
+                dateFormat.ShortDatePattern = "yyyy/MM/dd";
+                DateTime nowTime = Convert.ToDateTime(item.deadline, dateFormat);
+                statement.Bind(4, nowTime.Year.ToString() + "-" + nowTime.Month.ToString() + "-" + nowTime.Day.ToString());
+                statement.Step();
+            }
+        }
+
+        public void RemoveTodoItem(ListItem item)
+        {
+            using (var statement = conn.Prepare("DELETE FROM todolist WHERE id = ?;"))
+            {
+                statement.Bind(1, item.getId());
+                statement.Step();
+            }
+            this.allItems.Remove(item);
+            this.selectedItem = null;
+        }
+
+        public void UpdateTodoItem(string time, string title, string notes, string graph)
+        {
+            this.selectedItem.title = title;
+            this.selectedItem.notes = notes;
+            this.selectedItem.SetTime(time);
+            using (var statement = conn.Prepare("UPDATE todolist SET title = ?,notes = ?,deadline = ? WHERE id = ?;"))
+            {
+                statement.Bind(1, title);
+                statement.Bind(2, notes);
+
+                DateTimeFormatInfo dateFormat = new DateTimeFormatInfo();
+                dateFormat.ShortDatePattern = "yyyy/MM/dd";
+                DateTime nowTime = Convert.ToDateTime(this.selectedItem.deadline, dateFormat);
+                statement.Bind(3, nowTime.Year.ToString() + "-" + nowTime.Month.ToString() + "-" + nowTime.Day.ToString());
+                statement.Bind(4, graph);
+                statement.Bind(5, this.selectedItem.getId());
+
+                statement.Step();
+            }
+
+            this.selectedItem = null;
+        }
+
+    }
+}
