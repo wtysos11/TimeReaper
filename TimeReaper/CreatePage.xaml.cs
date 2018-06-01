9c03975fae5d5e9788584f9cb7c01afad3acd159
wty <wtysos11@163.com>
2018-06-01 13:34:10 +0800

    合法性验证完成

--- TimeReaper/CreatePage.xaml.cs
+++ TimeReaper/CreatePage.xaml.cs
@@ -48,6 +48,61 @@ namespace TimeReaper
         /*检查输入合法性,未完成*/
         bool checkValid()
         {
+            bool localTimeState = true;
+            bool localTitleState = true;
+            bool localNoteState = true;
+            if(CreateTitleInput.Text.Trim().Equals(String.Empty))
+            {
+                localTitleState = false;
+            }
+
+            if (CreateNoteInput.Text.Trim().Equals(String.Empty))
+            {
+                localNoteState = false;
+            }
+            DateTimeOffset dateTime = CreateDDLDateInput.Date;
+            TimeSpan dateTime2 = CreateDDLTimeInput.Time;
+            DateTimeOffset dateTimeOffset = new DateTimeOffset(dateTime.Year, dateTime.Month, dateTime.Day, dateTime2.Hours, dateTime2.Minutes, 0, new TimeSpan(8, 0, 0));
+            if(DateTimeOffset.Compare(dateTimeOffset,DateTimeOffset.Now)<=0)
+            {
+                localTimeState = false;
+            }
+            if (!localTitleState && !localNoteState && !localTimeState)
+            {
+                checkOut(0);
+                return false;
+            }
+            else if (!localTitleState && !localNoteState && localTimeState)
+            {
+                checkOut(1);
+                return false;
+            }
+            else if (!localTitleState && localNoteState && !localTimeState)
+            {
+                checkOut(2);
+                return false;
+            }
+            else if (!localTitleState && localNoteState && localTimeState)
+            {
+                checkOut(3);
+                return false;
+            }
+            else if (localTitleState && !localNoteState && !localTimeState)
+            {
+                checkOut(4);
+                return false;
+            }
+            else if (localTitleState && !localNoteState && localTimeState)
+            {
+                checkOut(5);
+                return false;
+            }
+            else if (localTitleState && localNoteState && !localTimeState)
+            {
+                checkOut(6);
+                return false;
+            }
+                
             return true;
         }
 
@@ -105,12 +160,22 @@ namespace TimeReaper
         /*报错信息*/
         private async void checkOut(int stateNum)
         {
+            if (stateNum == 0)
+            {
+                ContentDialog warningDialog = new ContentDialog()
+                {
+                    Title = "标题和备注的内容为空,且日期不合法（不能小于等于现在）",
+                    Content = "请输入标题和备注的内容，再检查日期",
+                    PrimaryButtonText = "Ok"
+                };
+                ContentDialogResult result = await warningDialog.ShowAsync();
+            }
             if (stateNum == 1)
             {
                 ContentDialog warningDialog = new ContentDialog()
                 {
-                    Title = "Title和Details的内容为空",
-                    Content = "请先输入Title和Details的内容",
+                    Title = "标题和备注的内容为空",
+                    Content = "请输入标题和备注",
                     PrimaryButtonText = "Ok"
                 };
                 ContentDialogResult result = await warningDialog.ShowAsync();
@@ -119,8 +184,8 @@ namespace TimeReaper
             {
                 ContentDialog warningDialog = new ContentDialog()
                 {
-                    Title = "Title的内容为空",
-                    Content = "请先输入Title的内容",
+                    Title = "标题的内容为空，日期不合法",
+                    Content = "请先输入标题，再检查日期",
                     PrimaryButtonText = "Ok"
                 };
                 ContentDialogResult result = await warningDialog.ShowAsync();
@@ -129,8 +194,8 @@ namespace TimeReaper
             {
                 ContentDialog warningDialog = new ContentDialog()
                 {
-                    Title = "Details的内容为空",
-                    Content = "请先输入Details的内容",
+                    Title = "标题为空",
+                    Content = "请先输入标题",
                     PrimaryButtonText = "Ok"
                 };
                 ContentDialogResult result = await warningDialog.ShowAsync();
@@ -139,8 +204,8 @@ namespace TimeReaper
             {
                 ContentDialog warningDialog = new ContentDialog()
                 {
-                    Title = "Title和Details的内容为空,且日期不合法（大于等于今天）",
-                    Content = "请先输入Title和Details的内容并且修改日期",
+                    Title = "备注为空，且日期不合法",
+                    Content = "请先输入备注的内容并且修改日期",
                     PrimaryButtonText = "Ok"
                 };
                 ContentDialogResult result = await warningDialog.ShowAsync();
@@ -149,23 +214,13 @@ namespace TimeReaper
             {
                 ContentDialog warningDialog = new ContentDialog()
                 {
-                    Title = "Title的内容为空，且日期不合法（需要大于等于今天）",
-                    Content = "请先输入Title的内容并且修改日期",
+                    Title = "备注为空",
+                    Content = "请先输入备注",
                     PrimaryButtonText = "Ok"
                 };
                 ContentDialogResult result = await warningDialog.ShowAsync();
             }
             if (stateNum == 6)
-            {
-                ContentDialog warningDialog = new ContentDialog()
-                {
-                    Title = "Details的内容为空，且日期不合法（需要大于等于今天）",
-                    Content = "请先输入Details的内容并且修改日期",
-                    PrimaryButtonText = "Ok"
-                };
-                ContentDialogResult result = await warningDialog.ShowAsync();
-            }
-            if (stateNum == 7)
             {
                 ContentDialog warningDialog = new ContentDialog()
                 {
